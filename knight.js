b1a64742d7876cbc638652ce44f5142d4042482d
bepsik777 <potocki.alex@gmail.com>
2023-07-06 11:53:09 +0200

    Delete knight.js file

--- knight.js
+++
@@ -1,9 +0,0 @@
-function knightFactory() {
-    function possibleMove(x,y) {
-        return [board[x+2][y+1], board[x+2][y-1], board[x+1][y+2], board[x+1][y-2], board[x-2][y-1], board[x-2][y+1], board[x-1][y-2], board[x-1][y+2]]
-    }
-
-    return {knightFactory}
-}
-
-export {knightFactory}
